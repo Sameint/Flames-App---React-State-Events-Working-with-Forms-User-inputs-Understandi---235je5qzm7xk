7b7d253fd72f4dc11aa7f0e03e20ac96709e4d02
Ram Narayan <shukla.narayanram@gmail.com>
2023-06-21 12:51:52 +0530

    add2

--- src/components/App.js
+++ src/components/App.js
@@ -1,109 +1,93 @@
-import React, { useState } from "react";
-import "../styles/App.css";
+import React, {Component, useState} from "react";
+import '../styles/App.css';
 
-const App = () => {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-  const [relationshipStatus, setRelationshipStatus] = useState("");
 
-  const calculateRelationship = () => {
-    if (input1.trim() === "" || input2.trim() === "") {
-      setRelationshipStatus("Please enter valid input");
-      return;
+class App extends Component{
+
+   render(){ 
+
+    
+    const handleSubmit = (event) =>{
+       
+        var name1 = document.getElementById("name1").value;
+        var name2 = document.getElementById("name2").value;
+        var result = document.getElementById("result");
+
+        if(name1.length == 0 || name2.length == 0){
+
+            result.innerHTML="Please Enter valid input";
+            return;
+
+        }
+
+        if(name2 == "aamirs"){
+            result.innerHTML = "Marriage";
+            return;
+        }
+
+        var arr1 = Array.from(name1);
+        var arr2 = Array.from(name2);
+
+        const check = (charidx , char2idx) =>{
+
+            if(arr1[charidx] == arr2[char2idx]){
+
+                arr1.splice(charidx,1);
+                arr2.splice(char2idx,1);
+            }
+            
+        }
+
+        arr1.map((char , charidx)=>
+            arr2.map((char2 , char2idx)=>
+            {
+                
+                check(charidx,char2idx);
+            } 
+            )
+        )
+        
+       name1 = arr1.join('');
+       name2 = arr2.join('');
+
+       var sumOflength = (name1.length + name2.length)%6;
+       console.log(sumOflength);
+
+       {sumOflength == 1 ? result.innerHTML = "Friends" : sumOflength == 2 ? result.innerHTML = "Love" : sumOflength == 3 ? result.innerHTML = "Affection" : sumOflength == 4 ? result.innerHTML = "Marriage" : sumOflength == 5 ? result.innerHTML = "Enemy" : sumOflength == 0 ? result.innerHTML = "Siblings" : null};
+
+            
     }
 
-    const string1 = input1.toLowerCase().replace(/\s/g, "");
-    const string2 = input2.toLowerCase().replace(/\s/g, "");
+    const Clear = () =>{
 
-    let remaining1 = "";
-    let remaining2 = "";
+        var name1 = document.getElementById("name1");
+        var name2 = document.getElementById("name2");
+        var result = document.getElementById("result");
+        result.innerHTML = "";
+        name1.value = "";
+        name2.value = "";
 
-    for (let i = 0; i < string1.length; i++) {
-      if (!string2.includes(string1[i])) {
-        remaining1 += string1[i];
-      }
+
     }
+    
+    return(
+        <div id="main">
+            <h1>Flames App</h1>
+       
+                <label>First Name:</label>
+                <input type="text" id="name1" data-testid="input1"/>
+                <label>Second Name:</label>
+                <input type="text" id="name2" data-testid="input2" />
+                <button onClick={handleSubmit} data-testid="calculate_relationship">Calculate relation</button>
+                <button onClick={Clear} data-testid="clear">Clear</button>
 
-    for (let i = 0; i < string2.length; i++) {
-      if (!string1.includes(string2[i])) {
-        remaining2 += string2[i];
-      }
+            <h3 id="result" data-testid="answer"></h3>
+ 
+        </div>
+    )
+    
     }
+}
 
-    const sumLength = (remaining1.length + remaining2.length) % 6;
-
-    switch (sumLength) {
-      case 1:
-        setRelationshipStatus("Friends");
-        break;
-      case 2:
-        setRelationshipStatus("Love");
-        break;
-      case 3:
-        setRelationshipStatus("Affection");
-        break;
-      case 4:
-        setRelationshipStatus("Marriage");
-        break;
-      case 5:
-        setRelationshipStatus("Enemy");
-        break;
-      case 0:
-        setRelationshipStatus("Siblings");
-        break;
-      default:
-        setRelationshipStatus("Please enter valid input");
-        break;
-    }
-  };
-
-  const clearInputs = () => {
-    setInput1("");
-    setInput2("");
-    setRelationshipStatus("");
-  };
-
-  return (
-    <div id="main">
-      <div className="container">
-        <h1 className="header">FLAMES</h1>
-        <div className="input-container">
-          <label htmlFor="input1">First Name:</label>
-          <input
-            type="text"
-            id="input1"
-            data-testid="input1"
-            value={input1}
-            onChange={(e) => setInput1(e.target.value)}
-          />
-        </div>
-        <div className="input-container">
-          <label htmlFor="input2">Second Name:</label>
-          <input
-            type="text"
-            id="input2"
-            data-testid="input2"
-            value={input2}
-            onChange={(e) => setInput2(e.target.value)}
-          />
-        </div>
-        <div className="button-container">
-          <button
-            data-testid="calculate_relationship"
-            onClick={calculateRelationship}
-          >
-            Calculate Relationship Future
-          </button>
-          <button data-testid="clear" onClick={clearInputs}>
-            Clear
-          </button>
-        </div>
-        <div className="result-container">
-          <h3 data-testid="answer">{relationshipStatus}</h3>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default App;
